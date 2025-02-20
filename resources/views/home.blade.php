71c5f257b06323730e4c2ec6b35c92971d5c9205
dithof <aditramadhan0001@gmail.com>
2025-02-20 10:43:11 +0700

    perubahan kedua

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app') @section('content')
+  <div class="container">
+  <h1>Home</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut architecto nesciunt facere explicabo obcaecati ullam amet, doloribus aspernatur et facilis ea esse a eligendi expedita provident molestiae labore officia id?</p>
+  </div>
+@endsection
